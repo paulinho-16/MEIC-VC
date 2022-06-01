ce5db8a92d0ba24a15ef1670af1e85e46804b4fc
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-06-01 16:58:14 +0100

    Update basic.py

--- Project 2/basic.py
+++ Project 2/basic.py
@@ -8,8 +8,21 @@ from torchvision import transforms, models
 import matplotlib.pyplot as plt
 from dataset import ImageClassificationDataset
 
+from types import SimpleNamespace
+
+config = SimpleNamespace()
+config.model_name = "resnet50"
+config.num_epochs = 5
+config.learning_rate = 0.05
+config.batch_size = 16
+config.num_workers = 2 
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
+
+########################################
+######################################## UTILS
+########################################
 
 def plotTrainingHistory(train_history, val_history):
     plt.subplot(2, 1, 1)
@@ -61,6 +74,9 @@ def showErrors(model, dataloader, num_examples=20):
         plt.imshow(inputs.permute(1, 2, 0).numpy())
     plt.show()
     
+########################################
+######################################## TRAINING
+########################################
 
 def epoch_iter(dataloader, model, loss_fn, optimizer=None, is_train=True):
     if is_train:
@@ -102,60 +118,75 @@ def epoch_iter(dataloader, model, loss_fn, optimizer=None, is_train=True):
 
     return total_loss / num_batches, accuracy_score(labels, preds)
 
-def train(model, model_name, num_epochs, train_dataloader, validation_dataloader, loss_fn, optimizer):
-  train_history = {'loss': [], 'accuracy': []}
-  val_history = {'loss': [], 'accuracy': []}
-  best_val_loss = np.inf
-  print("Start training...")
-  for t in range(num_epochs):
-      print(f"\nEpoch {t+1}")
-      train_loss, train_acc = epoch_iter(train_dataloader, model, loss_fn, optimizer)
-      print(f"Train loss: {train_loss:.3f} \t Train acc: {train_acc:.3f}")
-      val_loss, val_acc = epoch_iter(validation_dataloader, model, loss_fn, is_train=False)
-      print(f"Val loss: {val_loss:.3f} \t Val acc: {val_acc:.3f}")
+def train(model, train_dataloader, validation_dataloader, loss_fn):
+    train_history = {'loss': [], 'accuracy': []}
+    val_history = {'loss': [], 'accuracy': []}
 
-      # save model when val loss improves
-      if val_loss < best_val_loss:
-        best_val_loss = val_loss
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    best_val_loss = np.inf
+    print("Start training...")
+
+    for t in range(config.num_epochs):
+        print(f"\nEpoch {t+1}")
+        train_loss, train_acc = epoch_iter(train_dataloader, model, loss_fn, optimizer)
+        print(f"Train loss: {train_loss:.3f} \t Train acc: {train_acc:.3f}")
+
+        val_loss, val_acc = epoch_iter(validation_dataloader, model, loss_fn, is_train=False)
+        print(f"Val loss: {val_loss:.3f} \t Val acc: {val_acc:.3f}")
+
+        # Save best model
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            save_dict = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': t}
+            torch.save(save_dict, config.model_name + '_best_model.pth')
+
+        # Save latest model
         save_dict = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': t}
-        torch.save(save_dict, model_name + '_best_model.pth')
+        torch.save(save_dict, config.model_name + '_latest_model.pth')
 
-      # save latest model
-      save_dict = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': t}
-      torch.save(save_dict, model_name + '_latest_model.pth')
-
-      # save training history for plotting purposes
-      train_history["loss"].append(train_loss)
-      train_history["accuracy"].append(train_acc)
-
-      val_history["loss"].append(val_loss)
-      val_history["accuracy"].append(val_acc)
+        # Values for plotting
+        train_history["loss"].append(train_loss)
+        train_history["accuracy"].append(train_acc)
+        val_history["loss"].append(val_loss)
+        val_history["accuracy"].append(val_acc)
       
-  print("Finished")
-  return train_history, val_history
+    print("Finished")
+    return train_history, val_history
 
-class BasicVersion():
-    def __init__(self, model_name, train_dl, validation_dl, test_dl):
-        if model_name == 'vgg16':
-            model = models.vgg16(pretrained=True)
-            model.classifier[6] = nn.Linear(4096, 4)
-            model.to(device)
-            # print(model)
+########################################
+########################################
+########################################
 
-            num_epochs = 20
+class ClassificationResNet:
+    def __init__(self, train_dl, validation_dl):
+        model = models.resnet50(pretrained=True)
+        model.fc = nn.Linear(2048, 4)
 
-            loss_fn = nn.CrossEntropyLoss() # already includes the Softmax activation
-            optimizer_vgg = torch.optim.SGD(model.parameters(), lr=1e-3)
+        model.to(device)
 
-            vgg_train_history, vgg_val_history = train(model, model_name, num_epochs, train_dl, validation_dl, loss_fn, optimizer_vgg)
+        loss_function = nn.CrossEntropyLoss() # already includes the Softmax activation
 
-            plotTrainingHistory(vgg_train_history, vgg_val_history)
-            
-            self.test(model, test_dl, loss_fn)
-        elif model == 'ResNet':
-            model = models.resnet50(pretrained=True)
-            model.fc = nn.Linear(2048, 10)
-            model.to(device)
+        train_history, val_history = train(model, train_dl, validation_dl, loss_function)
+        plotTrainingHistory(train_history, val_history)
+
+        return model, loss_function
+
+class ClassificationVGG16:
+    def __init__(self, pre_trained = True):
+        self.pre_trained = pre_trained
+
+    def train(self, train_dl, validation_dl):
+        model = models.vgg16(pretrained=self.pre_trained)
+
+        model.classifier[6] = nn.Linear(4096, 4)
+        model.to(device)
+
+        loss_function = nn.CrossEntropyLoss()
+
+        train_history, val_history = train(model, train_dl, validation_dl, loss_function)
+        plotTrainingHistory(train_history, val_history)
+
+        return model, loss_function
 
     def test(self, best_model, test_dl, loss_fn):
         model = best_model 
@@ -168,36 +199,33 @@ class BasicVersion():
         print(f"\nTest Loss: {test_loss:.3f} \nTest Accuracy: {test_acc:.3f}")
 
         showErrors(model, test_dl)
-# load model from torchvision (with pretrained=True)
 
-# change the number of neurons in the last layer to the number of classes of the problem at hand (CIFAR10 dataset)
-train_transform = transforms.Compose([ # TODO: try another values/transformations
-    transforms.ToPILImage(),
-    transforms.Resize((200, 200)), # (400, 400)
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=45),
-    #transforms.CenterCrop(224),
-    transforms.ToTensor()
-    # transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
-])
+########################################
+########################################
+########################################
 
-validation_transform = transforms.Compose([ # TODO: try another values/transformations
-    transforms.ToPILImage(),
-    transforms.Resize((200, 200)), 
-    transforms.ToTensor()
-])
-
-test_transform = transforms.Compose([ # TODO: try another values/transformations
-    transforms.ToPILImage(),
-    transforms.Resize((200, 200)), # TODO: acho que não devia ter resize (ver link)
-    transforms.ToTensor()
-])
+transforms_dict = {
+    "train": transforms.Compose([ 
+                    transforms.ToPILImage(),
+                    transforms.Resize((200, 200)), 
+                    transforms.RandomHorizontalFlip(p=0.5),
+                    transforms.RandomRotation(degrees=45),
+                    transforms.ToTensor()
+                ]),
+    "validation": transforms.Compose([
+                    transforms.ToPILImage(),
+                    transforms.Resize((200, 200)), 
+                    transforms.ToTensor()
+                ]),
+    "test": transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Resize((200, 200)),
+                transforms.ToTensor()
+            ])
+}
 
 if __name__ == "__main__":
     # now we need to define a Dataloader, which allows us to automatically batch our inputs, do sampling and multiprocess data loading
-    batch_size = 4
-    num_workers = 2 # how many processes are used to load the data
-
     val_train_images = read_images('train.txt')
     test_images = read_images('test.txt')
 
@@ -207,12 +235,14 @@ if __name__ == "__main__":
     train_images = list(val_train_images[:train_ratio])
     validation_images = list(val_train_images[-validation_ratio:])
 
-    train_data =ImageClassificationDataset(train_images, train_transform)
-    validation_data = ImageClassificationDataset(validation_images, validation_transform)
-    test_data = ImageClassificationDataset(test_images, test_transform)
+    train_data =ImageClassificationDataset(train_images, transforms_dict['train'])
+    validation_data = ImageClassificationDataset(validation_images, transforms_dict['validation'])
+    test_data = ImageClassificationDataset(test_images, transforms_dict['test'])
 
-    train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=16, shuffle=True, drop_last=True)
-    validation_dataloader = torch.utils.data.DataLoader(validation_data, batch_size=16, shuffle=False, drop_last=False)
+    train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=config.batch_size, shuffle=True, drop_last=True)
+    validation_dataloader = torch.utils.data.DataLoader(validation_data, batch_size=config.batch_size, shuffle=False, drop_last=False)
     test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=True, drop_last=False)
 
-    BasicVersion("vgg16", train_dataloader, validation_dataloader, test_dataloader)
+    neural_network = ClassificationResNet(train_dataloader, validation_dataloader)
+    #model, loss_fn = neural_network.train(train_dataloader, validation_dataloader)
+    #neural_network.test(model, test_dataloader, loss_fn)
